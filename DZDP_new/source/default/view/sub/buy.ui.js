1756584103584bb9620b6f5b827ad3f305e2b2ae
wuwei0613 <2496661314@qq.com>
2015-10-08 21:30:15 +0800

    完成了第二、三个页面的界面

--- DZDP_new/source/default/view/sub/buy.ui.js
+++ DZDP_new/source/default/view/sub/buy.ui.js
@@ -1,11 +1,36 @@
 var app = sm("do_App");
-var do_Button_1 = ui("do_Button_1");
-var do_Button_2 = ui("do_Button_2");
+var sv_silder0 = ui("sv_silder0"); 
+var listdata = mm("do_ListData");
+var do_ImageView_1 = ui("do_ImageView_1");
+var do_ImageView_2 = ui("do_ImageView_2");
+var do_ImageView_3 = ui("do_ImageView_3");
 
-do_Button_1.on("touch",function(data, e){
-	app.openPage("source://view/sub/firstfile/barcode.ui","a");
-});
+sv_silder0.bindItems(listdata);
+listdata.addData([
+    {template : 0},
+    {template : 1},
+    {template : 2}
+]);
 
-do_Button_2.on("touch",function(data, e){
-	app.openPage("source://view/sub/firstfile/subfind.ui","a");
+sv_silder0.refreshItems();
+sv_silder0.on("indexChanged",function(index){
+
+	if(index == 0)
+	{
+		do_ImageView_1.source = "source://image/buy/oo.png";
+		do_ImageView_2.source = "source://image/buy/o.png";
+		do_ImageView_3.source = "source://image/buy/o.png";
+	}
+	else if(index == 1)
+	{
+		do_ImageView_1.source = "source://image/buy/o.png";
+		do_ImageView_2.source = "source://image/buy/oo.png";
+		do_ImageView_3.source = "source://image/buy/o.png";
+	}
+	else
+	{
+		do_ImageView_1.source = "source://image/buy/o.png";
+		do_ImageView_2.source = "source://image/buy/o.png";
+		do_ImageView_3.source = "source://image/buy/oo.png";
+	}
 });
